cc1e88cf2984380f6c396030280cfc330a89c9f2
soo5o <jungsk20@gmail.com>
2024-11-25 15:37:54 +0900

    feat: 앨범 목록 가져오기, 정렬 완료

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -3,10 +3,15 @@ package com.squarecross.photoalbum.repository;
 import com.squarecross.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     //앨범명으로 앨범 테이블을 검색하는 메서드
     Album findByAlbumName(String name); //쿼리 메서드
+    List<Album> findByAlbumNameContainingOrderByCreatedAtDesc(String keyword);
+    List<Album> findByAlbumNameContainingOrderByCreatedAtAsc(String keyword);
 
+    List<Album> findByAlbumNameContainingOrderByAlbumNameAsc(String keyword);
+    List<Album> findByAlbumNameContainingOrderByAlbumNameDesc(String keyword);
 }
--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -4,9 +4,12 @@ import com.squarecross.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     //Album 내 Photo 중 입력된 파일명을 가지고있는 앨범 리스트 추출
     //List<Photo> findByPhoto_FileName(String name);
     int countByAlbum_AlbumId(Long AlbumId);
+    List<Photo> findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(Long AlbumId);
 }
