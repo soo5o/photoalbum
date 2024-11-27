928fbca25ef670368c184b26078bca1fc6f332eb
soo5o <jungsk20@gmail.com>
2024-11-27 16:05:24 +0900

    feat: 사진 업로드, 다운로드 API 완료

--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
@@ -12,4 +13,5 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     //List<Photo> findByPhoto_FileName(String name);
     int countByAlbum_AlbumId(Long AlbumId);
     List<Photo> findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(Long AlbumId);
+    Optional<Photo> findByFileNameAndAlbum_AlbumId(String photoName, Long albumId);
 }
